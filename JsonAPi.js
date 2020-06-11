c6a9a5b822afc39ff8d261cebccb4a776b941180
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2020-06-11 16:54:23 -0700

    adding new files

--- JsonAPi.js
+++ JsonAPi.js
@@ -4,11 +4,29 @@ var getData = function(url, callback) {
     httpReq.open('GET', url);
     httpReq.responseType = 'application/json';
     
-    httpReq.addEventListener('progress', updateProgress);
+    //httpReq.addEventListener('progress', updateProgress);
     httpReq.setRequestHeader('Content-Type', 'text/plain');
 		httpReq.onreadystatechange = function() {
 			if (this.readyState === 4 && this.status === 200) {
         callback(null, this.responseText);
+        var json=JSON.parse(this.responseText);
+        //document.getElementById("reportData").innerHTML=JSON.stringify(json);
+        var mainContainer = document.getElementById("reportData");
+        var container=document.getElementById("Counties");
+  for (var i = 0; i < json.length; i++) {
+    var div = document.createElement("div");
+    var div1=document.createElement("div");
+     var Counties=json[i].Counties;
+    //console.log(Counties);
+    for(var j=0;j<Counties.length;j++){
+    	//console.log(Counties[j].CountyName);
+    div.innerHTML = 'State: ' + json[i].StateName+' CountyName: '+Counties[j].CountyName +' Population: '+ Counties[j].Population;
+    //div1.innerHTML='CountyName: ' +Counties[j].CountyName;
+   }
+   // container.appendChild(div1);
+    mainContainer.appendChild(div);
+    
+  }
       } 
       else {
         callback(`readyState = ${this.readyState}, status = ${this.status}`);
@@ -28,7 +46,7 @@ function getReportData() {
 	getData(remoteUrl, 
     function(err, data) {
       if (err === null) {
-      	console.log(`> Your document is loaded @ ${data} bytes`);
+      	console.log(`> Your document is loaded @ ${data.result} bytes`);
       }
       else {
       	console.log(`> Alert -- ${err}`);  
